0d2bda3b2d495ee9fcdf7a3a074b904b3506ac4d
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-01-20 12:27:25 -0800

    Finished problem 5

--- app.js
+++ app.js
@@ -109,9 +109,13 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(testArray) {
   //eslint-disable-line
+  var clone = testArray.slice(0);
+  var msg = 'The numbers '+ clone[0]+','+clone[1]+','+clone[2] +' have a product of '+ multiply(multiply(clone[0],clone[1])[0],clone[2])[0]+'.';
+  var result = [multiply(multiply(clone[0],clone[1])[0],clone[2])[0], msg];
+  return result;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
